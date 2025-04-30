41fbbbfdb7fac5aacff937d5c5e474035c37d5f1
Łukasz Flisak <lu.dev00@gmail.com>
2025-04-30 14:04:54 +0200

    Add Lesson, Access and Course models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    instructor_id = models.PositiveBigIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+
+
+class Access(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user_id = models.PositiveBigIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                "course_id", "user_id", name="course_user_unique"
+            )
+        ]
+
+
+class Lesson(models.Model):
+    name = models.CharField("Title", max_length=200)
+    content = models.TextField()
+    number = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[MinValueValidator(1)]
+    )
+    video = models.FileField('lesson-videos/', null=True, blank=True)
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['number']
